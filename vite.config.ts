37386dfea34e442e731542787721de41310f238b
DanilRev04 <danilrev04@gmail.com>
2025-05-04 00:47:07 +0300

    fix api

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:3000',
+                target: 'http://77.238.240.115:5085',
                 changeOrigin: true,
             },
         },
